35b9d13c2776fe9e237efb9d1ac1af663d87f75f
Pedro Barbosa Cordeiro <8323177+pedrocordeiro@users.noreply.github.com>
2022-11-10 14:22:23 -0300

    Add files via upload

---
+++ INSERT-CLIENTES.sql
@@ -0,0 +1,30 @@
+/*
+-- Query: SELECT `Cliente`.`Codigo`,
+    `Cliente`.`Nome`,
+    `Cliente`.`Cidade`,
+    `Cliente`.`UF`
+FROM `sql10556303`.`Cliente`
+LIMIT 0, 1000
+
+-- Date: 2022-11-10 14:20
+*/
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0001','Cliente 0001','Rio de Janeiro','RJ');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0002','Cloente 0002','Natal','RN');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0003','Cliente 0003','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0004','Cliente 0004','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0005','Cliente 0005','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0006','Cliente 0006','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0007','Cliente 0007','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0008','Cliente 0008','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0009','Cliente 0009','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0010','Cliente 0010','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0011','Cliente 0011','São Paulo','SP');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0012','Cliente 0012','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0013','Cliente 0013','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0014','Cliente 0014','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0015','Cliente 0015','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0016','Cliente 0016','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0017','Cliente 0017','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0018','Cliente 0018','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0019','Cliente 0019','Brasília','DF');
+INSERT INTO `` (`Codigo`,`Nome`,`Cidade`,`UF`) VALUES ('0020','Cliente 0020','Brasília','DF');
---
+++ INSERT-PRODUTOS.sql
@@ -0,0 +1,29 @@
+/*
+-- Query: SELECT `Produto`.`Codigo`,
+    `Produto`.`Descricao`,
+    `Produto`.`Valor`
+FROM `sql10556303`.`Produto`
+LIMIT 0, 1000
+
+-- Date: 2022-11-10 14:21
+*/
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0001','Produto 0001',1.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0002','Produto 0002',9.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0003','Produto 0003',2.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0004','Produto 0004',3.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0005','Produto 0005',4.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0006','Produto 0006',6.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0007','Produto 0007',7.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0008','Produto 0008',8.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0009','Produto 0009',9.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0010','Produto 0010',10.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0011','Produto 0011',11.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0012','Produto 0012',12.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0013','Produto 0013',13.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0014','Produto 0014',14.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0015','Produto 0015',15.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0016','Produto 0016',16.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0017','Produto 0017',17.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0018','Produto 0018',18.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0019','Produto 0019',19.99);
+INSERT INTO `` (`Codigo`,`Descricao`,`Valor`) VALUES ('0020','Produto 0020',20.99);
